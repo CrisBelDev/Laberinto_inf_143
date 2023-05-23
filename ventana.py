c1218accbd6214a2bcbe072459d732aeddad1010
Cristian Abel <cristtian.abel@gmail.com>
2023-05-23 10:31:49 -0400

    update ventana.py: se implemento POO y se agrego nuevas funciones

--- ventana.py
+++ ventana.py
@@ -1,70 +1,68 @@
 import pygame
 import sys
 
-# Inicializar pygame
-pygame.init()
+class Laberinto:
+    def __init__(self, laberinto):
+        self.laberinto = laberinto
+        self.ancho = len(laberinto[0])
+        self.alto = len(laberinto)
+        self.tamano_bloque = 50
+        self.ancho_ventana = self.ancho * self.tamano_bloque
+        self.alto_ventana = self.alto * self.tamano_bloque
+        self.posicion_jugador_x = 0
+        self.posicion_jugador_y = 0
 
-# Tamaño de la ventana del juego
-WIDTH = 640
-HEIGHT = 480
+        pygame.init()
+        self.pantalla = pygame.display.set_mode((self.ancho_ventana, self.alto_ventana))
 
-# Definir colores
-WHITE = (255, 255, 255)
-BLUE = (0, 0, 255)
+    def dibujar_laberinto(self):
+        for y in range(self.alto):
+            for x in range(self.ancho):
+                if self.laberinto[y][x] == 1:
+                    pygame.draw.rect(self.pantalla, (0, 0, 0), (x * self.tamano_bloque, y * self.tamano_bloque, self.tamano_bloque, self.tamano_bloque))
+                else:
+                    pygame.draw.rect(self.pantalla, (255, 255, 255), (x * self.tamano_bloque, y * self.tamano_bloque, self.tamano_bloque, self.tamano_bloque))
 
-# Definir el tamaño de los bloques y el laberinto
-BLOCK_SIZE = 40
-MAZE_WIDTH =10# WIDTH // BLOCK_SIZE
-MAZE_HEIGHT = 10#HEIGHT // BLOCK_SIZE
+    def dibujar_jugador(self):
+        pygame.draw.rect(self.pantalla, (255, 0, 0), (self.posicion_jugador_x * self.tamano_bloque, self.posicion_jugador_y * self.tamano_bloque, self.tamano_bloque, self.tamano_bloque))
 
-# Definir el laberinto
-maze = [
-    [1, 1, 1, 0, 1, 1, 1, 1, 1, 1],
-    [1, 0, 1, 1, 1, 0, 1, 0, 0, 1],
-    [1, 0, 0, 0, 0, 1, 1, 1, 1, 1],
-    [1, 1, 1, 1, 0, 1, 0, 1, 0, 1],
-    [1, 0, 0, 1, 0, 1, 0, 0, 0, 1],
-    [1, 1, 0, 1, 1, 1, 1, 1, 1, 1],
-    [1, 1, 1, 0, 0, 1, 0, 0, 0, 1],
-    [1, 0, 1, 1, 1, 1, 1, 1, 0, 1],
-    [1, 0, 0, 0, 0, 0, 0, 1, 0, 1],
-    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+    def mover_jugador(self, dx, dy):
+        nueva_posicion_x = self.posicion_jugador_x + dx
+        nueva_posicion_y = self.posicion_jugador_y + dy
+        if 0 <= nueva_posicion_x < self.ancho and 0 <= nueva_posicion_y < self.alto and self.laberinto[nueva_posicion_y][nueva_posicion_x] == 0:
+            self.posicion_jugador_x = nueva_posicion_x
+            self.posicion_jugador_y = nueva_posicion_y
+
+    def ejecutar_juego(self):
+        while True:
+            for evento in pygame.event.get():
+                if evento.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+                elif evento.type == pygame.KEYDOWN:
+                    if evento.key == pygame.K_UP:
+                        self.mover_jugador(0, -1)
+                    elif evento.key == pygame.K_DOWN:
+                        self.mover_jugador(0, 1)
+                    elif evento.key == pygame.K_LEFT:
+                        self.mover_jugador(-1, 0)
+                    elif evento.key == pygame.K_RIGHT:
+                        self.mover_jugador(1, 0)
+
+            self.pantalla.fill((0, 0, 0))
+            self.dibujar_laberinto()
+            self.dibujar_jugador()
+            pygame.display.update()
+
+
+# Ejemplo de uso
+laberinto = [
+    [0, 1, 0, 1, 1],
+    [0, 0, 0, 1, 0],
+    [0, 1, 0, 0, 0],
+    [0, 1, 0, 1, 0],
+    [0, 0, 0, 0, 0]
 ]
 
-# Inicializar la posición del jugador
-player_pos = [1, 1]
-
-# Crear la ventana del juego
-screen = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("Laberinto")
-
-# Bucle principal del juego
-while True:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            sys.exit()
-
-        # Movimiento del jugador
-        elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_UP and maze[player_pos[1] - 1][player_pos[0]] == 0:
-                player_pos[1] -= 1
-            elif event.key == pygame.K_DOWN and maze[player_pos[1] + 1][player_pos[0]] == 0:
-                player_pos[1] += 1
-            elif event.key == pygame.K_LEFT and maze[player_pos[1]][player_pos[0] - 1] == 0:
-                player_pos[0] -= 1
-            elif event.key == pygame.K_RIGHT and maze[player_pos[1]][player_pos[0] + 1] == 0:
-                player_pos[0] += 1
-
-    # Dibujar el laberinto en la ventana
-    screen.fill(WHITE)
-    for y in range(MAZE_HEIGHT):
-        for x in range(MAZE_WIDTH):
-            if maze[y][x] == 1:
-                pygame.draw.rect(screen, BLUE, (x * BLOCK_SIZE, y * BLOCK_SIZE, BLOCK_SIZE, BLOCK_SIZE))
-
-    # Dibujar al jugador en la ventana
-    pygame.draw.rect(screen, (255, 0, 0), (player_pos[0] * BLOCK_SIZE, player_pos[1] * BLOCK_SIZE, BLOCK_SIZE, BLOCK_SIZE))
-
-    # Actualizar la ventana
-    pygame.display.flip()
+juego = Laberinto(laberinto)
+juego.ejecutar_juego()
